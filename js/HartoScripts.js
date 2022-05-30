7f0f29bc118eceee07aff35bc340b0672e18d2bc
Antar Kuri <perklapoint@gmail.com>
2022-05-30 02:51:41 -0300

    H&M + GOES + dots

--- js/HartoScripts.js
+++ js/HartoScripts.js
@@ -4,9 +4,10 @@ $(document).ready(function(){
     lazyLoad: 'ondemand',
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true,
+    arrows: false,
+    dots: true,
     infinite: true,
-    speed: 500,
+    speed: 750,
     cssEase: 'ease-out',
     prevArrow: '<img src="assets/img/H_e-UI-flecha-prev.png" class="slide-arrow prev-arrow">',
     nextArrow: '<img src="assets/img/H_e-UI-flecha-next.png" class="slide-arrow next-arrow">'
